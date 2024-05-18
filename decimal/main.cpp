ce296d82938ca8abd5ea8ce2dd5f1375b96ef863
TalonAbuser001 <162663694+TalonAbuser001@users.noreply.github.com>
2024-05-18 10:50:03 -0500

    Add files via upload

--- decimal/main.cpp
+++ decimal/main.cpp
@@ -1,44 +1,20 @@
-#include "listadecimal.h"
+#include "decimal.h"
 
-void menu();
+int main()
+{
+    decimal n1(7654321);
+    decimal n2(7891234);
+    decimal n3;
 
-int main() {
-    ListaDecimal A;
-    char op = '0';
-    while (op < '5') {
-        menu();
-        cin >> op;
-        switch (op) {
-            case '1': A.pedirDatos();
-                break;
-            case '2': A.quitarDatos();
-                break;
-            case '3': A.imprimir1();
-                break;
-            case '4': A.imprimir2();
-                break;
-            case '5': 
-                break;
-            default: cout << "\n\n\tERROR EN OPCION";
-                op = '0';
-        }
-    }
-    ListaDecimal B(A); // Invocar el constructor de copia
-    cout << "\nLista A = "; A.imprimir1();
-    cout << "\nLista B = "; B.imprimir1();
+    cout << "\nn1 es " << n1;
+    cout << "\nn2 es " << n2;
+    cout << "\nn3 es " << n3;
 
-    ListaDecimal C = A + B; // Realizar la suma de listas
-    cout << "\nLista C = A + B = " << C;
+    n3 = n1 + n2;
+
+    cout << "\n\n" << n1 << " + " << n2 << " = " << n3;
+
+    cout<<"\n\n";
 
     return 0;
-}
-
-void menu() {
-    cout << "\n\n\t*** MENU PRINCIPAL ***";
-    cout << "\n\n1. Entrar datos a la lista";
-    cout << "\n2. Borrar datos de la lista";
-    cout << "\n3. Imprimir lista de prim a ultimo";
-    cout << "\n4. Imprimir lista de ultimo a primero";
-    cout << "\n5. Salir del menu";
-    cout << "\n\n\tEscoja opcion: ";
 }
